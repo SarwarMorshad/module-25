528967a94076e472d7b89dcba3396abc0abcd36f
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-08-24 09:51:05 +0200

    Module 25: Learning Events

--- script/main.js
+++ script/main.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+//   document.getElementById().addEventListener('event type', handler)
+document.getElementById("btn-make-green").addEventListener("click", function makeGreen() {
+  document.body.style.backgroundColor = "green";
+});
+
+document.getElementById("btn-make-gold").addEventListener("click", function makeGold() {
+  document.body.style.backgroundColor = "gold";
+});
